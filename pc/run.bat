1f5ba6423642317325a2912743b1949fcecc69a2
maxesharp <70987132+maxesharp@users.noreply.github.com>
2026-09-15 13:46:54 -0500

    update for new .ps1 script name

--- pc/run.bat
+++ pc/run.bat
@@ -4,7 +4,7 @@ set "SCRIPT_DIR=%~dp0"
 if "%SCRIPT_DIR:~-1%"=="\" set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-    "Unblock-File -LiteralPath '%SCRIPT_DIR%\launch.ps1' -ErrorAction SilentlyContinue"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\launch.ps1" %*
+    "Unblock-File -LiteralPath '%SCRIPT_DIR%\launcher.ps1' -ErrorAction SilentlyContinue"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\launcher.ps1" %*
 
 pause
